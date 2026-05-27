6bbcb8f1771739466f2b4ca03d16841dedef8483
Beata Wierzbicka <b.wierzbicka90@gmail.com>
2026-05-27 15:30:47 -0400

    logic is finished

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -5,9 +5,16 @@ class RestaurantsController < ApplicationController
   end
 
   def new
+    @restaurant = Restaurant.new
   end
 
   def create
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      redirect_to restaurant_path(@restaurant.id)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def show
@@ -17,5 +24,9 @@ class RestaurantsController < ApplicationController
 
   def set_restaurant
     @restaurant = Restaurant.find(params[:id])
+  end
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
   end
 end
